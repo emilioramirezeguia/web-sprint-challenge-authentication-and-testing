295222c8442da1f9783e9725b0f4a6be4d4bd2ac
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-21 12:15:58 -0700

    Register users with hashed passwords

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,27 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const Auth = require("./auth-model");
 
-router.post('/register', (req, res) => {
-  // implement registration
+// Register a new user
+router.post("/register", (req, res) => {
+  const user = req.body;
+
+  // hash the password
+  const rounds = 8;
+  const hash = bcryptjs.hashSync(user.password, rounds);
+
+  user.password = hash;
+  // save the user to the database
+  Auth.add(user)
+    .then((user) => {
+      res.status(201).json({ user });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
